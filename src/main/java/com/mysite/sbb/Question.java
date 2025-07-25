37af8228f58d90f760c60543d56db70e2dc56e78
jiseon723 <dighxxx12@gmail.com>
2025-07-25 16:42:34 +0900

    02-04, ë entity

---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //자동응답
+    private Integer id;
+
+    @Column(length = 200) // VARCHAR처럼 글의 길이를 제한
+    private String subject;
+
+    @Column(columnDefinition = "TEXT") // 텍스트 타입으로 긴글 저장.
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) // 주인을 지정, 질문을 삭제하면 안의 답변도 같이 삭제하겠다.
+    private List<Answer> answerList;
+}
