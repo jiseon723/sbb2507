4094dcb2033269d38603dcf68c2f97c70b920c01
jiseon723 <dighxxx12@gmail.com>
2025-07-29 17:47:52 +0900

    interpret_3

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer/*id*/> {
-    Question findBySubject(String subject);
-    Question findBySubjectAndContent(String subject, String content);
-    List<Question> findBySubjectLike(String subject);
+    Question findBySubject(String subject); // Question 데이터베이스에서 subject를 찾을 것
+    Question findBySubjectAndContent(String subject, String content); //Question 데이터베이스에서 subject와 content를 찾을 것
+    List<Question> findBySubjectLike(String subject); // Question 데이터베이스에서 subject를 찾아 수정할 것
 }
