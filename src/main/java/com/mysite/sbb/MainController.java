c52f507af36a298437343b9c7c59ec2b93315b37
jiseon723 <dighxxx12@gmail.com>
2025-07-30 18:07:54 +0900

    2-8 root, redirect

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -11,4 +11,10 @@ public class MainController {
     public String index() {
         return "안녕하세요 sbb에 오신 것을 환영합니다.";
     }
+
+    // 리다이렉트
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
