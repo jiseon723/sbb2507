c071c9fcdc6a4a0156b37e1d9e49ed062bd4f887
jiseon723 <dighxxx12@gmail.com>
2025-07-31 16:21:50 +0900

    answer Controller

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -1,8 +1,23 @@
 package com.mysite.sbb.answer;
 
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/answer")
+@RequiredArgsConstructor
 @Controller
 public class AnswerController {
+    private final QuestionService questionService;
 
+    @PostMapping(value = "/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+        Question question = this.questionService.getQuestion(id);
+
+        // TODO: 답변을 저장한다.
+        return "";
+    }
 }
