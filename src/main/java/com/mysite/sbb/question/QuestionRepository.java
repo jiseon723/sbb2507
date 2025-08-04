a429fa2f7f8b75e60d0c27b00346b788beeb6c54
jiseon723 <dighxxx12@gmail.com>
2025-08-04 14:45:08 +0900

    paging

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -1,5 +1,7 @@
 package com.mysite.sbb.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer/*id*
     Question findBySubject(String subject); // Question 데이터베이스에서 subject를 찾을 것
     Question findBySubjectAndContent(String subject, String content); //Question 데이터베이스에서 subject와 content를 찾을 것
     List<Question> findBySubjectLike(String subject); // Question 데이터베이스에서 subject를 찾아 수정할 것
+    Page<Question> findAll(Pageable pageable);
 }
