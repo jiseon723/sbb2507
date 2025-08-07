7042414ff64b4e35c5b598661f8f42856e8ffe4a
jiseon723 <dighxxx12@gmail.com>
2025-08-07 14:38:30 +0900

    userr form

---
+++ src/main/java/com/mysite/sbb/user/UserController.java
@@ -0,0 +1,33 @@
+package com.mysite.sbb.user;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserController userController;
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup_form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+        //todo: 패스워드 일치 여부 검토
+
+        //todo: 회원가입 처리
+
+        return "redirect:/";
+    }
+}
